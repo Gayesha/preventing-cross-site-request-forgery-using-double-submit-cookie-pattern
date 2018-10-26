9346d73c3f21281a051c33d98ee0597e9d70c9ed
Gayesha <gayesha.suraweera@my.sliit.lk>
2018-10-26 22:43:16 +0530

    Update PropertyConstants.java

--- double-submit-cookie-pattern-master/src/main/java/com/sliit/ssd/csrfapp/constants/PropertyConstants.java
+++ double-submit-cookie-pattern-master/src/main/java/com/sliit/ssd/csrfapp/constants/PropertyConstants.java
@@ -1,8 +1,6 @@
 package com.sliit.ssd.csrfapp.constants;
 
-/**
- * Created by dinukshakandasamanage on 9/5/18.
- */
+
 public class PropertyConstants {
 
     public static final String APP_PROPERTIES_FILE = "application.properties";
